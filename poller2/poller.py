f36de4e082e040b15a458ad34003aac11e216d04
Alexei <alexei@entropy.energy>
2022-10-10 22:11:16 +1100

    Updates now instead of reload

--- poller2/poller.py
+++ poller2/poller.py
@@ -32,6 +32,36 @@ def defaults(D,**args):
     args.update(D)
     return args
 
+class ButtonDiv(jp.Div):
+    '''
+    Hold voting buttons
+    '''
+
+    def __init__(self, **kwargs):
+        super().__init__(**defaults(
+            kwargs,
+            delete_flag=False,
+            classes="w-full flex flex-col pt-5 gap-2",
+        ))
+
+    def setButtons(self, poll, sid=None):
+        self.delete_components()
+
+        vote = poll['votes'].get(sid)
+        for k in poll['choices'].split():
+            if vote==k:
+                state = "bg-indigo-500"
+            else:
+                state = "bg-blue-300 hover:bg-blue-500 focus:ring-blue-400"
+            btn=jp.Button(a=self,
+                          value=k,
+                          classes=f"items-center {state} rounded-md border p-4 focus:ring-2 focus:ring-offset-2",
+                          click=castVote,
+                          )
+            jp.Span(a=btn, text=k, classes="text-white text-3xl")
+            if vote==k:
+                btn.disabled = True
+
 
 @jp.SetRoute('/')
 @jp.SetRoute('/{pollid:int}')
@@ -63,21 +93,25 @@ def pollee(request):
         #
         # Buttons
         #
-        div = jp.Div(a=col, classes="w-full flex flex-col pt-5 gap-2")
-        vote = poll['votes'].get(sid)
-        for k in poll['choices'].split():
-            if vote==k:
-                state = "bg-indigo-500"
-            else:
-                state = "bg-blue-300 hover:bg-blue-500 focus:ring-blue-400"
-            btn=jp.Button(a=div,
-                          value=k,
-                          classes=f"items-center {state} rounded-md border p-4 focus:ring-2 focus:ring-offset-2",
-                          click=castVote,
-                          )
-            jp.Span(a=btn, text=k, classes="text-white text-3xl")
-            if vote==k:
-                btn.disabled = True
+        wp.button_div = ButtonDiv(a=col)
+        wp.button_div.setButtons(poll, sid)
+
+
+        # self.button_div = jp.Div(a=col, classes="w-full flex flex-col pt-5 gap-2")
+        # vote = poll['votes'].get(sid)
+        # for k in poll['choices'].split():
+        #     if vote==k:
+        #         state = "bg-indigo-500"
+        #     else:
+        #         state = "bg-blue-300 hover:bg-blue-500 focus:ring-blue-400"
+        #     btn=jp.Button(a=self.button_div,
+        #                   value=k,
+        #                   classes=f"items-center {state} rounded-md border p-4 focus:ring-2 focus:ring-offset-2",
+        #                   click=castVote,
+        #                   )
+        #     jp.Span(a=btn, text=k, classes="text-white text-3xl")
+        #     if vote==k:
+        #         btn.disabled = True
 
     return wp
 
@@ -85,9 +119,12 @@ async def castVote(self, msg):
 
     pollid = msg.page.pollid
     poll = current_polls[pollid]
-    poll['votes'][msg.session_id] = msg.value
+    if 'value' in msg:
+        poll['votes'][msg.session_id] = msg.value
     print(pollid, msg)
-    await msg.page.reload()
+    msg.page.button_div.setButtons(poll,msg.session_id)
+    #await msg.page.reload()
+    await msg.page.update()
 
     for page in jp.WebPage.instances.values():
         if page.pollid == pollid and page.role == "poller":
@@ -115,10 +152,6 @@ class ChartDiv(jp.Div):
             #classes="q-pa-xs",
         ))
 
-    #     self.setup()
-
-    # def setup(self):
-
         chart_def ={
             'chart':{'type':'bar'},
             'title':{'text':''},
@@ -140,40 +173,6 @@ class ChartDiv(jp.Div):
 
         self.chart = jp.HighCharts(a=self, classes='w-full', options=chart_def, delete_flag=False)
 
-    # def setChart(self, poll):
-
-    #     choices = poll['choices'].split()
-    #     votes = poll['votes'].values()
-    #     # totals
-    #     C = Counter()
-    #     for v in votes:
-    #         print(C,v)
-    #         C.update([v])
-    #     counts = [C[c] for c in choices]
-
-    #     chart_def ={
-    #         'chart':{'type':'bar'},
-    #         'title':{'text':''},
-    #         'xAxis':{'categories':choices, 'labels': {'style':{'fontSize':'24px'}}},
-    #         'yAxis':{'title':{'text':'Votes'}, 'allowDecimals':False},
-    #         'plotOptions': {
-    #             'series': {'animation': False},
-    #             'bar': {
-    #                 'grouping': False,
-    #                 'groupPadding': 0,
-    #                 'maxPointWidth': 0,
-    #                 'pointPadding': 0,
-    #                 'showInLegend': False,
-    #             }
-    #         },
-    #         'credits':{'enabled':False},
-    #         'series':[{'data':counts}],
-    #     }
-
-    #     self.delete_components()
-    #     self.chart = jp.HighCharts(a=self, classes='w-full', options=chart_def, delete_flag=False)
-    #     #self.chart = chart
-    #     #print(chart)
 
     def updateChart(self, poll):
 
@@ -182,7 +181,7 @@ class ChartDiv(jp.Div):
         # totals
         C = Counter()
         for v in votes:
-            print(C,v)
+            print('before update',C,v,votes)
             C.update([v])
         counts = [C[c] for c in choices]
 
@@ -262,7 +261,9 @@ async def choiceChange(self,msg):
             await page.update()
             #await page.chart.update()
         else:
-            await page.reload()
+            page.button_div.setButtons(poll)
+            #await page.reload()
+            await page.update()
         #jp.run_task(msg.page.update())
 
 
